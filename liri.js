8b50713aeba827dd3cde21c3aa30333c3f2b55d2
sjconst <stephanie.c.lake@gmail.com>
2019-09-25 14:19:01 -0700

    all basic functionality working

--- liri.js
+++ liri.js
@@ -3,69 +3,125 @@ var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
 var axios =  require("axios");
 var moment = require("moment");
-var spotify = new Spotify(keys.spotify)
-var input;
+var fs = require("fs");
+var spotify = new Spotify(keys.spotify);
+var secondArg = process.argv[2].toLowerCase();
+var command, input;
 {
     console.log("Choose from four commands to use this app: concert-this + name of an artist, spotify-this-song + name of a song, movie-this + name of a movie, or do-what-it-says");
 }
-if(process.argv.length > 3){
-    var command = process.argv[2].toLowerCase();
-    console.log(command);
-    if(process.argv.length > 4){
-        input = [];
-        for(var i = 3; i < process.argv.length; i++){
-            input.push(process.argv[i].toLowerCase());
-        };
-    }
-}
-console.log(input);
-if(command === "concert-this"){
-//     `node liri.js concert-this <artist/band name here>`
-//    * This will search the Bands in Town Artist Events API (`"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"`) for an artist and render the following information about each event to the terminal:
-//      * Name of the venue
-//      * Venue location
-//      * Date of the Event (use moment to format this as "MM/DD/YYYY")
-    console.log("test")
-} else if(command === "spotify-this-song"){
-    /* If no song is provided then your program will default to "The Sign" by Ace of Base. */
-    var song = input.join(" ");
-    spotify
-    .search({type: 'track', query: song, limit: 1 })
-    .then(data => {
-        var results = data.tracks.items[0];
-        console.log(`Song name: ${results.name} \n Album: ${results.album.name} \n Artist(s): ${results.album.artists[0].name} \n Preview link: ${results.external_urls.spotify}`)     
+if(process.argv.length > 3 && secondArg != "do-what-it-says"){  
+    command = process.argv[2].toLowerCase();
+    input = [];
+    for(var i = 3; i < process.argv.length; i++){
+        input.push(process.argv[i].toLowerCase());
+    };
+    processCommand(input, command);
+} else if(secondArg === "do-what-it-says"){
+    fs.readFile("random.txt", "utf8", (error, data) => {
+        if(error) {
+            return console.log(error)
+        }
+        content = data.split(",");      
+        command = content[0];
+        input = content[1];      
+        processCommand(input, command);
+    })    
+} 
+function getConcert(input){
+    var artist = input;
+    var url = `https://rest.bandsintown.com/artists/${artist}/events?app_id=codingbootcamp&date=upcoming`;
+    axios
+    .get(url)
+    .then(response => {
+        console.log(`${artist} is playing in the following upcoming venues:`)
+        var responseArr = response.data;        
+        responseArr.forEach(element => {
+            var date = moment(element.datetime).format("MM/DD/YYYY");         
+            if(element.venue.country === "United States"){
+                var location = element.venue.region;
+            } else {
+                var location = element.venue.country;
+            }
+            console.log(`Venue: ${element.venue.name}, Location: ${element.venue.city}, ${location}, Date: ${date}`)
+        });
     })
     .catch(error => {
-        console.log(error);
-    });
-} else if(command === "movie-this"){
-    console.log("a movie")
-} else if(command === "do-what-it-says"){
-    console.log("test")
+        if(error.response){
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+        } else if(error.request){
+            console.log(error.request);
+        } else{
+            console.log("error", error.message)
+        }
+        console.log(error.config);
+    }) 
+};
+function getSpotify(input){
+    if(!input){
+        var trackID = "0hrBpAOgrt8RXigk83LLNE";
+        console.log("You didn't choose a song, so we picked for you!")
+        spotify
+        .request(`https://api.spotify.com/v1/tracks/${trackID}`)
+        .then(data => {            
+            console.log(`Song name: ${data.name} \n Album: ${data.album.name} \n Artist(s): ${data.album.artists[0].name} \n Preview link: ${data.preview_url}`) 
+        })
+        .catch(error => console.log(error))
+    } else {        
+        spotify
+        .search({
+            type: 'track', query: input, limit: 1 
+        })
+        .then(data => {
+            var results = data.tracks.items[0];        
+            console.log(`Song name: ${results.name} \n Album: ${results.album.name} \n Artist(s): ${results.album.artists[0].name} \n Preview link: ${results.external_urls.spotify}`)     
+        })
+        .catch(error => console.log(error));
+    }
+}; 
+function getMovie(input){      
+    //Input: a string
+    //Output: if no input, console.log details for Mr. Nobody, else console.log details for input
+    if(!input){
+        var movie = "Mr. Nobody"
+        console.log("You didn't pick a movie, so we picked for you!")
+    } else {
+        var movie = input;
+    }    
+    var url = `http://www.omdbapi.com/?apikey=trilogy&t=${movie}`;
+    axios
+    .get(url)
+    .then(response => {
+        var results = response.data;       
+        console.log(`Title: ${results.Title}\n Year: ${results.Year} \n imdb Rating: ${results.imdbRating}/10\n ${results.Ratings[1].Source}: ${results.Ratings[1].Value}\n Produced in: ${results.Country}\n Language: ${results.Language}\n Plot: ${results.Plot}\n Actors: ${results.Actors}`);     
+    })
+    .catch(error => {
+        if(error.response){
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+        } else if(error.request){
+            console.log(error.request);
+        } else{
+            console.log("error", error.message)
+        }
+        console.log(error.config);
+    })    
 }
-
-/* 
- 
-
-`node liri.js movie-this '<movie name here>'`
-   * This will output the following information to your terminal/bash window:
-     ```
-       * Title of the movie.
-       * Year the movie came out.
-       * IMDB Rating of the movie.
-       * Rotten Tomatoes Rating of the movie.
-       * Country where the movie was produced.
-       * Language of the movie.
-       * Plot of the movie.
-       * Actors in the movie.
-     ```
-   * If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
-     * If you haven't watched "Mr. Nobody," then you should: <http://www.imdb.com/title/tt0485947/>
-     * It's on Netflix!
-   * You'll use the `axios` package to retrieve data from the OMDB API. Like all of the in-class activities, the OMDB API requires an API key. You may use `trilogy`.
-
-`node liri.js do-what-it-says`
-   * Using the `fs` Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
-     * It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
-     * Edit the text in random.txt to test out the feature for movie-this and concert-this.
-*/
+function tryAgain(){
+    //No input. Outputs message when called. 
+    console.log("Sorry, I didn't understand that. Please try again");
+}
+function processCommand(input, command){
+    if(command === "movie-this"){  
+        getMovie(input);
+    } else if(command === "spotify-this-song"){
+        getSpotify(input);
+    } else if(command === "concert-this"){
+        getConcert(input);
+    } else {
+        tryAgain();
+    }    
+}
